b3c6999d37db29796c7525f30fe03768d4152f94
Dnalor4 <rhmailutile@gmail.com>
2025-11-27 17:54:07 +0100

    Implement Santa's light instructions and sum function
    Added two functions for Santa's light instructions and a sum function.

--- christmas_lights_kata.py
+++ christmas_lights_kata.py
@@ -1,3 +1,4 @@
+#première compréhension des instruction de santa
 def instruction(list_l, instruction, map_points = (0,0,0,0)):
 	if instruction == 'on':
 		for i in range(map_points[0], map_points[2]+1):
@@ -17,6 +18,7 @@ def instruction(list_l, instruction, map_points = (0,0,0,0)):
 		print('error')	
 	return list_l
 
+#seconde compréhension des instrucion de santa
 def instruction2(list_l, instruction, map_points = (0,0,0,0)):
 	if instruction == 'on':
 		for i in range(map_points[0], map_points[2]+1):
@@ -37,6 +39,9 @@ def instruction2(list_l, instruction, map_points = (0,0,0,0)):
 		print('error')	
 	return list_l
 
+#réalise les instuction demander par Santa  sur les plage demander:
+#dans le premier type d'instruction :'on' True, 'off' False, 'toggle' inverse l'état
+#dans le second type d'instruction :'on' +1, 'off' -1 (minimum 0), 'toggle' +2
 def santas_instruction(list_l , instruction_demander):
 	list_l = instruction_demander(list_l,'on',(887,9,959,629))
 	list_l = instruction_demander(list_l,'on',(454,398,844,448))
@@ -49,6 +54,7 @@ def santas_instruction(list_l , instruction_demander):
 	list_l = instruction_demander(list_l,'toggle',(831,394,904,860))
 	return list_l
 
+#Fait la somme des éléments d'une liste de dimension 2
 def somme(list_l):
 	s = 0
 	for i in range(len(list_l)):
